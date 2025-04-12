1869ba5e10691fbb2226c39a2b33ff8a694eeb40
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2025-04-12 16:59:10 -0300

    adiciona tailwind ao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Footer from './components/Footer'
+import Nav from './components/Nav'
+import Header from './components/Header'
+
+import './css/index.css'
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Nav/>
+        <Routes>
+
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <> 
+        <footer className="text-white text-center p-4 mt-4 rounded-t-lg" style={{
+            background: `linear-gradient(to right, #1a002b, #0e0015)`
+        }}>
+            <p>&copy; 2025 - Melodia Todos os direitos reservados - Melodia</p>
+        </footer>
+
+    </>
+  )
+}
+
+export default Footer
